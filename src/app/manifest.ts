c30091fc10d02f9d35f04f687a0fbc59829c48df
Pranav S <pranav@bus3.in>
2025-06-23 20:58:35 +0530

    icon path issue fixed

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: 'public/icon.png',
+        src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
       }
